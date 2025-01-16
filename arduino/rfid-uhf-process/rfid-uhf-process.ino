55bf940d73fc5fd4de10dabd20d9ebed6d900060
aabeltran0321 <aa.beltran0321@gmail.com>
2025-01-16 18:21:21 +0800

    update codes

--- arduino/rfid-uhf-process/rfid-uhf-process.ino
+++ arduino/rfid-uhf-process/rfid-uhf-process.ino
@@ -40,46 +40,107 @@ constexpr uint8_t RST_PIN = 48;          // Configurable, see typical pin layout
 constexpr uint8_t SS_PIN = 53;         // Configurable, see typical pin layout above
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
-long prevMillis;
+
+
+//LED INIT
+
+#include "FastLED.h"
+
+/*************************************************
+   ANDROID AMBILIGHT APPLICATION ARDUINO SKETCH
+
+   set following values to your needs
+ *************************************************/
+
+#define INITIAL_LED_TEST_ENABLED true
+#define INITIAL_LED_TEST_BRIGHTNESS 16  // 0..255
+#define INITIAL_LED_TEST_TIME_MS 500  // 10..
+
+// Number of leds in your strip.
+// Tested with 500 leds and is fine (despite the warning)
+// We will use a maximum of 300
+#define MAX_LEDS 82
+
+// type of your led controller, possible values, see below
+#define LED_TYPE WS2812B 
+
+// 3 wire (pwm): NEOPIXEL BTM1829 TM1812 TM1809 TM1804 TM1803 UCS1903 UCS1903B UCS1904 UCS2903 WS2812 WS2852
+//               S2812B SK6812 SK6822 APA106 PL9823 WS2811 WS2813 APA104 WS2811_40 GW6205 GW6205_40 LPD1886 LPD1886_8BIT 
+// 4 wire (spi): LPD8806 WS2801 WS2803 SM16716 P9813 APA102 SK9822 DOTSTAR
+
+// For 3 wire led stripes line Neopixel/Ws2812, which have a data line, ground, and power, you just need to define DATA_PIN.
+// For led chipsets that are SPI based (four wires - data, clock, ground, and power), both defines DATA_PIN and CLOCK_PIN are needed
+
+// DATA_PIN, or DATA_PIN, CLOCK_PIN
+#define LED_PINS 6        // 3 wire leds
+//#define LED_PINS 6, 13  // 4 wire leds
+
+#define COLOR_ORDER GRB  // colororder of the stripe, set RGB in hyperion
+
+#define OFF_TIMEOUT 8000    // ms to switch off after no data was received, set 0 to deactivate
+
+#define BRIGHTNESS 255                      // maximum brightness 0-255
+#define DITHER_MODE BINARY_DITHER           // BINARY_DITHER or DISABLE_DITHER
+#define COLOR_TEMPERATURE CRGB(255,255,255) // RGB value describing the color temperature
+#define COLOR_CORRECTION  TypicalLEDStrip   // predefined fastled color correction
+//#define COLOR_CORRECTION  CRGB(255,255,255) // or RGB value describing the color correction
+
+// Baudrate, higher rate allows faster refresh rate and more LEDs
+#define BAUD_RATE 115200      // use 115200 for ftdi based boards
+
+
+/*************************************************
+   ANDROID AMBILIGHT APPLICATION ARDUINO SKETCH
+
+   no user changes needed
+ *************************************************/
+
+// Adalight sends a "Magic Word" before sending the pixel data
+uint8_t prefix[] = {'A', 'd', 'a'}, hi, lo, chk, i;
+
+unsigned long endTime;
+
+// Define the array of leds
+CRGB leds[MAX_LEDS];
+
+// set color to all leds
+void showColor(const CRGB& led) {
+  #if MAX_LEDS > 1
+  LEDS.showColor(led);
+  #endif
+}
+
+// switch of leds
+void switchOff() {
+  #if MAX_LEDS > 1
+  memset(leds, 0, MAX_LEDS * sizeof(struct CRGB));
+  FastLED.show();
+  #endif
+}
+
+
 void setup() {
 	Serial.begin(115200);		// Initialize serial communications with the PC
-  Serial1.begin(115200);
-  Serial2.begin(115200);
-  Serial3.begin(115200);
-
+  Serial1.begin(115200);	
+  Serial2.begin(115200);	
+  Serial3.begin(115200);	
 	while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
 	SPI.begin();			// Init SPI bus
 	mfrc522.PCD_Init();		// Init MFRC522
 	mfrc522.PCD_DumpVersionToSerial();	// Show details of PCD - MFRC522 Card Reader details
 	Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+  led_init();
 }
 
 void loop() {
 	mfrc522_process();
+  led_process();
   if((millis()-prevMillis) > 1000){
-    uhf_rfid_process1();
-    uhf_rfid_process2();
-    uhf_rfid_process3();
+    uhf_rfid_process();
     prevMillis = millis();
   }
 
 }
-
-void mfrc522_process(){
-  if (mfrc522.PICC_IsNewCardPresent()) {
-    if (mfrc522.PICC_ReadCardSerial()) {
-      Serial.print("PROCESS");
-      byte scannedUID[4]; // Store the scanned UID
-      for (byte i = 0; i < mfrc522.uid.size; i++) {
-        scannedUID[i] = mfrc522.uid.uidByte[i];
-        Serial.print(scannedUID[i] < 0x10 ? " 0" : " ");
-        Serial.print(scannedUID[i], HEX);
-      }
-      Serial.println();
-    }
-	}
-}
-
 
 void uhf_rfid_process1(){
   // Define the data to send
@@ -135,7 +196,6 @@ void uhf_rfid_process2(){
   }
 }
 
-
 void uhf_rfid_process3(){
   // Define the data to send
   byte data[] = {0xBB, 0x00, 0x27, 0x00, 0x03, 0x22, 0x27, 0x10, 0x83, 0x7E};
@@ -160,5 +220,72 @@ void uhf_rfid_process3(){
     response.trim();                           // Remove trailing space
     Serial.print("uhf3-");
     Serial.println(response);                  // Print the response as a single string
+  }
+}
+
+void mfrc522_process(){
+  if (mfrc522.PICC_IsNewCardPresent()) {
+    if (mfrc522.PICC_ReadCardSerial()) {
+      Serial.print("PROCESS");
+      byte scannedUID[4]; // Store the scanned UID
+      for (byte i = 0; i < mfrc522.uid.size; i++) {
+        scannedUID[i] = mfrc522.uid.uidByte[i];
+        Serial.print(scannedUID[i] < 0x10 ? " 0" : " ");
+        Serial.print(scannedUID[i], HEX);
+      }
+      Serial.println();
+    }
+  }
+}
+
+void led_init(){
+  int ledCount = MAX_LEDS;
+
+  #if MAX_LEDS > 1
+    FastLED.addLeds<LED_TYPE, LED_PINS, COLOR_ORDER>(leds, ledCount);
+  #endif
+  
+  // color adjustments
+  FastLED.setBrightness ( BRIGHTNESS );
+  FastLED.setTemperature( COLOR_TEMPERATURE );
+  FastLED.setCorrection ( COLOR_CORRECTION );
+  FastLED.setDither     ( DITHER_MODE );
+
+  // initial RGB flash
+  #if INITIAL_LED_TEST_ENABLED == true
+  for (int v=0;v<INITIAL_LED_TEST_BRIGHTNESS;v++)
+  {
+    showColor(CRGB(255,0,0));  
+    delay(INITIAL_LED_TEST_TIME_MS/2/INITIAL_LED_TEST_BRIGHTNESS);
+  }
+  for (int v=0;v<INITIAL_LED_TEST_BRIGHTNESS;v++)
+  {
+    showColor(CRGB(0,255,0));  
+    delay(INITIAL_LED_TEST_TIME_MS/2/INITIAL_LED_TEST_BRIGHTNESS);
+  }
+  for (int v=0;v<INITIAL_LED_TEST_BRIGHTNESS;v++)
+  {
+    showColor(CRGB(0,0,255));  
+    delay(INITIAL_LED_TEST_TIME_MS/2/INITIAL_LED_TEST_BRIGHTNESS);
+  }
+  #endif
+  showColor(CRGB(0, 0, 0));
+}
+
+void led_process(){
+  while(Serial.available()){
+    char c = Serial.read();
+    if (c == 'r'){
+      showColor(CRGB(0,255,0));  
+    }
+    if (c == 'g'){
+      showColor(CRGB(255,0,0));  
+    }
+    if (c == 'b'){
+      showColor(CRGB(0,0,255));  
+    }
+    if (c == 'x'){
+      showColor(CRGB(0,0,0));  
+    }
   }
 }
